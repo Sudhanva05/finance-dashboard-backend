45428f68a1c125bf12fb134d51efa59928315b01
Sudhanva05 <raosudhanvaj@email.com>
2026-04-02 22:01:38 +0530

    add get_current_user for JWT authentication and rbac

--- app/core/security.py
+++ app/core/security.py
@@ -1,19 +1,56 @@
-from passlib.context import CryptContext
-from jose import jwt
-from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.models.user import User
+from jose import jwt, JWTError
 from app.core.config import SECRET_KEY, ALGORITHM
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+#  Get current user from token
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
+
+
+#  Role-based access
+def require_role(allowed_roles: list):
+    def role_checker(user: User = Depends(get_current_user)):
+        if user.role not in allowed_roles:
+            raise HTTPException(status_code=403, detail="Not authorized")
+        return user
+    return role_checker
+
+
+#  Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-#passwoed and verify password
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str , hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password,hashed_password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
-#JWT Token 
-def create_access_token(data: dict) -> dict:
+
+#  JWT token
+def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(hours=2)
     to_encode.update({"exp": expire})
