849f8deb9185b0fa1a58ff566d07e90b8c288cbf
Sudhanva05 <raosudhanvaj@email.com>
2026-04-02 20:12:27 +0530

    added user auth in main app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.routes import auth
 
 app = FastAPI(
     title="Finance Dashboard Backend",
     description="Backend for finance records with role-based access control",
     version="1.0.0"
 )
+
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+ 
 
 @app.get("/")
 def root():
