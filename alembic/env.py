263a1dcab9da7d085ac9d443b7e219e9da8e4c53
Sudhanva05 <raosudhanvaj@email.com>
2026-04-02 16:05:25 +0530

    add user model and initial migration with alembic

--- alembic/env.py
+++ alembic/env.py
@@ -27,6 +27,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 
 from app.db.base import Base
+from app.models import user  # noqa: F401
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
