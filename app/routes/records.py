d47a5279eb51321bb404c4c71970e746b45ac6f5
Sudhanva05 <raosudhanvaj@email.com>
2026-04-06 14:51:42 +0530

    add filters to record endpoints

--- app/routes/records.py
+++ app/routes/records.py
@@ -5,6 +5,7 @@ from app.db.session import get_db
 from app.models.record import Record
 from app.schemas.record import RecordCreate
 from app.core.security import get_current_user, require_role
+from typing import Optional
 
 router = APIRouter()
 
@@ -33,7 +34,21 @@ def create_record(
 #  Get all records (any logged user)
 @router.get("/")
 def get_records(
+    category: Optional[str] = None,
+    min_amount: Optional[float] = None,
+    max_amount: Optional[float] = None,
     db: Session = Depends(get_db),
     user = Depends(get_current_user)
 ):
-    return db.query(Record).all()
+    query = db.query(Record)
+
+    if category:
+        query = query.filter(Record.category == category)
+
+    if min_amount:
+        query = query.filter(Record.amount >= min_amount)
+
+    if max_amount:
+        query = query.filter(Record.amount <= max_amount)
+
+    return query.all()
